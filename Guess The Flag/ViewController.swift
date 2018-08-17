7a99c6807c49129bc0721d0ae86b0684c5752491
Jameka Echols <jameka.echols@gmail.com>
2018-08-16 20:25:14 -0500

    added png files, added UIButtons and aligned them.

--- Guess The Flag/ViewController.swift
+++ Guess The Flag/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
